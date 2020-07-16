06a6f7d437fd6104aef418d1d5c01f4475baacde
tim wu <timwuuk@gmail.com>
2020-07-16 23:40:17 +0100

    stragegy

--- index.php
+++ index.php
@@ -2,6 +2,42 @@
 
 require 'vendor/autoload.php';
 
-use App\TurkeySub;
+class LogToFile implements Logger
+{
+  public function log($data)
+  {
+    var_dump('log to file ' . $data);
+  }
+}
 
-(new App\VeggieSub)->make();
+class LogToDatabase implements Logger
+{
+  public function log($data)
+  {
+    var_dump('log to database ' . $data);
+  }
+}
+
+class LogToXWebService implements Logger
+{
+  public function log($data)
+  {
+    var_dump('log to sass ' . $data);
+  }
+}
+
+interface Logger
+{
+  public function log($data);
+}
+
+class App
+{
+  public function useLog($data, Logger $logger = null)
+  {
+    $newLogger = $logger ?: new LogToFile;
+    $newLogger->log($data);
+  }
+}
+
+(new App)->useLog('hollo');
