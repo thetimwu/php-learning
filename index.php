f186f1f088fa666dc42b8761d7fb0b708b98f11b
tim wu <timwuuk@gmail.com>
2020-10-14 15:32:42 +0100

    add pdo connecting to mysql

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require 'vendor/autoload.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require __DIR__ . "/vendor/autoload.php";
+
+    use App\TestDBConn;
+
+    $tdb = new TestDBConn();
+    $tdb->fetchByTitle("new title", "this is body");
+    $tdb->addPost("my new title", "new body", "test url");
+    ?>
+</body>
+
+</html>
